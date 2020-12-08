0caaf34ad22676c7039b36ada7e64c57c7fe5132
profesorek96 <krzysztof.los.dev@gmail.com>
2020-12-08 21:36:20 +0100

    Listing 1 (czesc 2 artykulu) Program wyświetlający podłoże oraz postać gracza w lewym górnym rogu ekranu

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pgzrun  # importujemy bibliotekę
 # Pygame Zero
 
+# tworzymy obiekt klasy Actor, gracza
+# pierwszy argument - nazwa grafiki
+# drugi argument - krotka, pozycja
+gracz = Actor('p3_stand', (0, 0))
+
+
 def draw():  # funkcja rysująca
     screen.clear()  # metoda czyszcząca ekran
     screen.fill('#cff4f7')
@@ -9,6 +15,7 @@ def draw():  # funkcja rysująca
         screen.blit('grass', (i * 70, screen.height - 70))
         # rysuje grafikę o nazwie
         # grass w punkcie 0,0
+    gracz.draw()  # rysujemy obiekt gracza
 
 
 def update():  # funkcja wykonuje się co klatkę
