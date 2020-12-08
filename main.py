a481701c420883362581e45e3434c1446dbe1129
profesorek96 <krzysztof.los.dev@gmail.com>
2020-12-08 21:47:34 +0100

    Listing 8 (część 2 artykułu) Program ukazujący ekran startowy, animację gracza oraz skok gracza

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pgzrun  # importujemy bibliotekę
+
 # Pygame Zero
 
 # tworzymy obiekt klasy Actor, gracza
@@ -13,10 +14,19 @@ stan_gry = 0  # zmienna reprezentująca
 # 2 - ekran końcowy
 
 klatka = 0  # zmienna globalna
-
-
-# reprezentujaca która klatka
+# reprezentująca, która klatka
 # animacji ma być aktualnie odtworzona
+
+skok = 0  # zmienna globalna
+# reprezentująca szybkość oraz kierunek
+# przemieszczania się gracza
+
+blokada_skoku = 0  # zmienna globalna
+
+
+# będąca flaga
+# 0 - skok jest możliwy
+# 1 - skok nie jest możliwy
 
 def draw():  # funkcja rysująca
     global stan_gry  # ustawienie zmiennej stan_gry
@@ -42,12 +52,66 @@ def draw():  # funkcja rysująca
 
 
 def update():  # funkcja wykonuje się co klatkę
-    global stan_gry  # ustawienie zmiennej stan_gry
+    global stan_gry
+    # ustawienie zmiennej stan_gry jako globalnej
+    global skok
+    # ustawienie zmiennej skok jako globalnej
+    global blokada_skoku
+    # ustawienie zmiennej blokada_skoku jako globalnej
     if keyboard.SPACE:  # sprawdza czy został
         # wciśnięty klawisz spacji
         # jeśli tak
-        stan_gry = 1
+        if stan_gry == 0:
+            # jeśli gra nie rozpoczęła się
+            blokada_skoku = 1  # zablokuj skok
+            clock.schedule_unique(zwolnienie_blokady, 0.3)
+            # odblokowanie skoku nastąpi po 0.3 sekundy
+        stan_gry = 1  # rozpocznij grę
+        if blokada_skoku == 0:
+            # jeśli skok możliwy do wykonania
+            # wartość zero
+            skok = -18
+            # ustaw szybkość skoku na 18 pikseli
+            blokada_skoku = 1
+            # zablokuj możliwość ponownego skoku
     animacja()
+    # uruchom funkcje odpowiedzialną za animacje postaci
+    skok_opadanie()
+    # uruchom funkcję kontrolującą skok oraz opadanie
+
+
+def zwolnienie_blokady():  # funkcja odpowiedzialna
+    # za zwalnaianie blokady skoku
+    # zmienia wartość zmiennej blokada_skoku na zero
+    global blokada_skoku
+    # ustawienie zmiennej stan_gry jako globalnej
+    blokada_skoku = 0
+    # zwalnianie blokady skoku
+
+
+def skok_opadanie():  # funkcja odpowiedzialna za
+    # skok oraz opadanie
+    global skok
+    # ustawienie zmiennej skok jako globalnej
+    global klatka  # zmienna globalna
+    # decydujaca, która klatka
+    # animacji ma zostać aktualnie wyświetlona
+    if skok != 0:  # jeśli skok aktywny
+        klatka = 0  # zablokowanie animacji
+        gracz.y += skok
+        # gracz zmienia swoją pozycją o wartość
+        # zmiennej skok
+    if gracz.y >= 484:  # jeśli gracz dotknął podłoża
+        zwolnienie_blokady()
+        # następuje zwolnienie blokady skoku
+        skok = 0
+        # wartość zero gwarantuje, że nasz bohater
+        # będzie stał w miejscu tuż nad podłożem
+    if gracz.y <= 250:  # jeśli gracz znajduje się
+        # wyżej niż wartość 250 na osi y 
+        # zacznij opadanie
+        skok *= (-1)  # zmiana kierunku 
+        # poruszania się sprite
 
 
 def animacja():  # funkcja odpowiedzialna za
