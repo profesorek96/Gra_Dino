496588db2d1decd3ea0cfaab1b9ccde5029d1958
profesorek96 <krzysztof.los.dev@gmail.com>
2020-12-08 21:23:06 +0100

    Listing 4. Kod rysujący niebieskie tło oraz wyświetlający grafikę grass.png na pozycji 0,0

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ def draw():  # funkcja rysująca
     screen.clear()  # metoda czyszcząca ekran
     screen.fill('#cff4f7')
     # metoda wypełniająca kolorem
+    screen.blit('grass', (0, 0))
+    # rysuje grafikę o nazwie grass w punkcie 0,0
 
 
-def update():  # funkcja wykonuje 
-    # się co klatkę
+def update():  # funkcja wykonuje się co klatkę
     pass  # pusta funkcja update
 
 
-pgzrun.go()  # Linia ta jest konieczna 
+pgzrun.go()  # Linia ta jest konieczna
 # aby okno nie zostało zamknięte
