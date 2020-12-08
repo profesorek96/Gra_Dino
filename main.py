7d1ac6df19895e55ef5818421df189fec055e58f
profesorek96 <krzysztof.los.dev@gmail.com>
2020-12-08 21:42:26 +0100

    Listing 6 (część 2 artykułu) Program generujący okno gry wraz z tłem, podłożem, sprite’m gracza. Dodatkowo na środku wyświetla napis Wciśnij spację

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ import pgzrun  # importujemy bibliotekę
 # drugi argument - krotka, pozycja
 gracz = Actor('p3_stand', (100, 484))
 
+stan_gry = 0  # zmienna reprezentująca
+
+
+# stan gry
+# 0 - gra nie rozpoczęta
+# 1 - rozpoczyna rozgrywkę
+# 2 - ekran końcowy
 
 def draw():  # funkcja rysująca
+    global stan_gry  # ustawienie zmiennej stan_gry
+    # jako zmiennej globalnej
     screen.clear()  # metoda czyszcząca ekran
     screen.fill('#cff4f7')
     # metoda wypełniająca kolorem
@@ -16,13 +25,24 @@ def draw():  # funkcja rysująca
         # rysuje grafikę o nazwie
         # grass w punkcie 0,0
     gracz.draw()  # rysujemy obiekt gracza
+    if stan_gry == 0:
+        # jeśli gra się nie rozpoczęła
+        screen.draw.text(
+            "Wciśnij spację",
+            center=(screen.width / 2, screen.height / 2),
+            color="orange",
+            fontsize=60
+        )
+        # wyświetla napis "Wciśnij spację"
+        # na środku ekranu
 
 
 def update():  # funkcja wykonuje się co klatkę
+    global stan_gry  # ustawienie zmiennej stan_gry
     if keyboard.SPACE:  # sprawdza czy został
         # wciśnięty klawisz spacji
         # jeśli tak
-        print('spacja')  # wypisuje napis
+        stan_gry = 1
 
 
 pgzrun.go()  # Linia ta jest konieczna
