66ee535e2a1b09958a3226d25bd5b6cd5788146f
profesorek96 <krzysztof.los.dev@gmail.com>
2020-12-08 21:15:36 +0100

    Listing  2. Działanie funkcji draw() i update()

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pgzrun  # importujemy bibliotekę 
 # Pygame Zero
 
+def draw():  # funkcja rysująca
+    print('rysuj')
+
+def update():  # funkcja wykonuje
+    # się co klatkę
+    print('KLATKA')
+
 pgzrun.go()  # Linia ta jest konieczna 
 # aby okno nie zostało zamknięte
